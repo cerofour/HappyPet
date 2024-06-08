a3a6d9b79885233a352133b693236de8626c0316
Diego Llacsahuanga <llacsahuanga.buques@gmail.com>
2024-06-08 16:42:56 -0500

    Rewrite of the navigation bar

--- www/scripts.js
+++ www/scripts.js
@@ -1,25 +1,9 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const toggleButton = document.querySelector('.toggle-button');
-    const navbarLinks = document.querySelector('.navbar-links');
+function showSidebar(){
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'flex';
+}
 
-    toggleButton.addEventListener('click', () => {
-        navbarLinks.classList.toggle('active');
-    });
-});
-
-let counter = 1;
-const radioButtons = document.querySelectorAll('input[name="radio-btn"]');
-
-setInterval(() => {
-    document.getElementById(`img-${counter}`).checked = true;
-    counter++;
-    if (counter > 4) {
-        counter = 1;
-    }
-}, 5000);
-
-radioButtons.forEach((radioButton, index) => {
-    radioButton.addEventListener('change', () => {
-        counter = index + 1;
-    });
-});
+function hideSidebar(){
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'none';
+}
