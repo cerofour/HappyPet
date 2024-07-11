47e7fe032dcf6c04e9975db21a8c288101100e65
Dereck <derecknova23@gmail.com>
2024-07-11 18:26:38 -0500

    Added menu on mobile view

--- www/scripts.js
+++ www/scripts.js
@@ -11,4 +11,23 @@ function showSlides() {
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
   setTimeout(showSlides, 4000); // Change image every 2 seconds
-} 
+}
+
+//const defaultLinksDisplay = document.querySelector('.hideOnMobile').style.display;
+document.addEventListener('DOMContentLoaded', () => {
+  const button = document.querySelector('.main-nav-bar button');    const navBar = document.querySelector('.main-nav-bar');
+  const links = document.querySelectorAll('.mobileMenu');
+      button.addEventListener('click', () => {
+        
+        const websiteName = document.querySelector('.first-nav-element');
+
+        if (websiteName.style.display === '')
+          websiteName.style.display = 'none';
+        else
+          websiteName.style.display = ''
+
+        navBar.classList.toggle('full-screen-menu');
+          for (const link of links)
+        link.style.display = (link.style.display == 'block') ? 'none' : 'block';
+    });
+});
